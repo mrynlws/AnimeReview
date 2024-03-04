9d1c2ecdeb67e96a937102b4c2ad740edc293ce2
Yannie <ianneab07@gmail.com>
2024-03-05 00:38:16 +0800

    commented errors

--- app/page.js
+++ app/page.js
@@ -11,26 +11,11 @@
 // };
 
 const Dashboard = async () => {
-  const getReviews = async () => {
-    try {
-      const res = await fetch("/api/Anime", {
-        cache: "no-store",
-      });
-      // const res = await fetch("http://localhost:3000/api/Anime", {
-      //   cache: "no-store",
-      // });
-  
-      return res.json();
-    } catch (error) {
-      console.log("Failed to get reviews", error);
-    }
-  };
-
-  const { animes } = await getReviews();
+  // const { animes } = await getReviews();
 
   return (
     <div className="flex lg:grid grid-cols-2 xl:grid-cols-4 justify-around p-5">
-      {animes.map((anime, index) => (
+      {/* {animes.map((anime, index) => (
         <div key={index} className="container p-5">
           <div className=" bg-slate-200 w-rounded">
           <div>{anime.title}</div>
@@ -39,7 +24,7 @@ const Dashboard = async () => {
           <div>{anime.rating}</div>
           </div>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 };
